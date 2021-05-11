da74eab442a5e6dc1917eada9f8023687e6fb044
shilagit1234 <shilagit1234@gmail.com>
2021-05-11 12:59:31 +0530

    add change

--- c/apps.py
+++ c/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 
 class CConfig(AppConfig):
     name = 'c'
+from django.apps import AppConfig
+
+from django.apps import AppConfig
+
+
+class CConfig(AppConfig):
+    name = 'c'
+
+class CConfig(AppConfig):
+    name = 'c'
